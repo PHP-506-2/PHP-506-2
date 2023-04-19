1544b58af9be4374e80eaf87707fff4635eeac42
Choihyukjae <wo4013@naver.com>
2023-04-19 12:41:08 +0900

    0419( list update define 변경필요 )최혁재

--- src/pet_profile_bar.php
+++ src/pet_profile_bar.php
@@ -2,9 +2,11 @@
 define( "URL_DB","D:\project\src\common\db_common.php" );
 include_once( URL_DB );
 
-
-
-
+    $arr
+    = array(
+        "pet_no" => 1
+    );
+    $ee = pet_info($arr["pet_no"]);
 ?>
 
 <!DOCTYPE html>
@@ -19,30 +21,29 @@ include_once( URL_DB );
 <body>
     <div class="wrap">
         <div class="profile_bar">
-        <div class = "v-line">
-        </div>
-            <a class="home" href="petlist_list.php"> <img src="./pet.png" alt=""></a>
-            <p class="day">함께한지 +<? echo $dog_day ?></p>
-            <progress value="<? echo round($chinmildo) ?>" max="100"></progress>
+            <div class = "v-line"></div>
+            <a class="home" href="petlist_list.php"> <img src="../img/pet.png" alt=""></a>
+            
+            <p class="day"><?php echo $ee["pet_name"]; ?>과 함께한지 +<? echo $dog_day ?></p>
+            <progress id="progress" value="<? echo round($chinmildo) ?>" max="100"></progress>
             <? echo round($chinmildo)."%" ?>
+            <br>
+            <div class ="sp_1"> <p>♥이백이와의 친밀도♥</p></div>
+
             <div class="profile_main">
                 <div class="profile_img">
-
+                <img class="two" src="../img/2100.jpg" alt="">
                 </div>
                 <div class="profile_con">
-                    <?php  
-                        $arr
-                        = array(
-                            "pet_no" => 1
-                        );
-                        $ee = pet_info($arr["pet_no"]);
-                    ?>
-
-                    <p>이름 : <?php echo $ee["pet_name"]; ?> </p>
-                    <p>입양일 :<?php echo $ee["pet_birth"]; ?> </p>
-                    <p>성별 :<?php echo $ee["pet_gender"]; ?> </p>
+                    <p class = "p_1" >이름 : <?php echo $ee["pet_name"]; ?> </p>
+                    <p class = "p_1" >입양일 :<?php echo $ee["pet_birth"]; ?> </p>
+                    <p class = "p_1" >성별 :<?php echo $ee["pet_gender"]; ?> </p>
                 </div>
             </div>
+        </div>
+        <div class="lk_1">
+        <a class="btn_1" href="https://www.zooseyo.or.kr/zooseyo_or_kr.html?" target=_blank>종합유기견보호센터</a>
+        <a class="btn_1" href="https://www.karma.or.kr/" target=_blank>동물구조관리협회</a>
         </div>
     </div>
     
