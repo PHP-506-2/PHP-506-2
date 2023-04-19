8783442256bf075d25f5ce222de6adc92b46559d
yovjvng <yoyo0249@naver.com>
2023-04-19 17:06:32 +0900

    0419( up profile_insert )백유정

--- src/petlist_profile_insert.php
+++ src/petlist_profile_insert.php
@@ -5,6 +5,15 @@ include_once( URL_DB );
 
 $http_method = $_SERVER["REQUEST_METHOD"];
 
+
+
+// 처리 완료 후 홈 페이지로 이동
+if ( $http_method === "POST" ) {
+    $arr_post = $_POST;
+    pet_list_insert( $arr_post );
+    header( "Location: petlist_list.php" );
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,10 +22,40 @@ $http_method = $_SERVER["REQUEST_METHOD"];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PROFILE INSERT</title>
     <link rel="stylesheet" href="../css/common.css">
 </head>
 <body>
-    
+    <h1>Pet Profile</h1>
+    <form method="post" action="petlist_profile_insert.php">
+            <div class="con_main">
+                <label for="name">이름 : </label>
+                <input type="text" name="pet_name"  id="name" required placeholder="이름" autocomplete="off">
+                <br>
+                <br>
+                <label for="birth">입양일 : </label>
+                <input type="date" name="pet_birth"  id="birth" required>
+                <br>
+                <br>
+                <select name="select" id="select">
+                    <option value="0" selected>남</option>
+                    <option value="1">여</option>
+                </select>
+            </div>
+            <br>
+            <br>
+            <div class="btn_wrap">
+                <button class="btn_fix" type="submit">
+                    수정
+                </button>
+                <button href="board_list.php">
+                    취소
+                </button>
+            </div>
+        </form>
+
+
+
+
 </body>
 </html>
--- src/petlist_update.php
+++ src/petlist_update.php
@@ -1,7 +1,6 @@
 <?php
 include_once( "./common/define.php" );
 include_once( URL_DB );
-include_once( URL_HEADER );
 
 $http_method = $_SERVER["REQUEST_METHOD"];
 
@@ -37,7 +36,9 @@ if( $http_method === "GET")
         header( "Location: petlist_detail.php?list_no=".$arr_post["list_no"] );
         exit();
     }
-
+ // pet정보 테이블 전체 정보 획득
+ $a = 1; //함수가 pet_info( &$param_arr )라서 pet_no = 1의 값을 가져오기 위해
+ $result_pet_info = pet_info( $a ); //숫자 넣으면 error
 
 
 ?>
@@ -50,63 +51,67 @@ if( $http_method === "GET")
     <link rel="stylesheet" href="../css/common.css">
     <link rel="stylesheet" href="../css/pet_profile_bar.css">
     <link rel="stylesheet" href="../css/petlist_update.css">
-    <title>Document</title>
+    <title>UPDATE</title>
 </head>
 <body>
-    <div class="wrap_mian">
-        <div class="petlist_profile_container">
-            <?php 
-             include_once 'pet_profile_bar.php';
-            ?>
-        </div>
-    
-        <div class="update_form">
-            <select name="select" id="select">
-                <option value="0" selected>진행예정</option>
-                <option value="1">진행중</option>
-                <option value="2">진행완료</option>
-                <option value="3">진행실패</option>
-            </select>
+    <div class="petlist_main_border">
+        <? include_once( URL_HEADER ); ?>
+        <div class="wrap_mian">
+            <div class="petlist_profile_container">
+                <?php 
+                include_once 'pet_profile_bar.php';
+                ?>
+            </div>
         
-            <form class="update_form_put" method="post" action="petlist_update.php">
-                <!-- <div class="upput"> -->
-                    <label for="bno">번호 : </label>
-                    <input type="text" name="list_no" id="bno" value="<?php echo $result_info['list_no'] ?>" readonly>
-                    <br>
-                    <label for="title">제목 : </label>
-                    <input type="text" name="list_title"  id="title" required placeholder="제목" autocomplete="off"
-                    value="<?php echo $result_info['list_title'] ?>">
-                    <br>
-                    <label for="title">시작일자 : </label>
-                    <input type="datetime-local" name="list_start"  id="title" required 
-                    value="<?php echo $result_info['list_start'] ?>">
-                    <br>
-                    <label for="title">기한일자 : </label>
-                    <input type="datetime-local" name="list_end"  id="title" required
-                    value="<?php echo $result_info['list_end'] ?>">
-                    <br>
-                    <label for="title">장소 : </label>
-                    <input type="text" name="list_location"  id="title" required placeholder="장소" autocomplete="off"
-                    value="<?php echo $result_info['list_location'] ?>">
-                    <br>
-                    <br>
-                    <label class="contents" for="contents">내용 : </label>
-                    <textarea class="input_contents" name="list_contents" id="contents" spellcheck="false" cols="48" rows="15" >
-                        <?php echo $result_info['list_contents'] ?>
-                    </textarea>
-                <!-- </div>     -->
-                <div class="btn_wrap">
-                    <a class="btn_fix" type="submit">
-                        수정
-                    </a>
-                    <a class="trash" href='petlist_delete.php '>
-                        삭제
-                    </a>
-                    <a class="btn_fix" href='petlist_list.php'>
-                        취소
-                    </a>
-                </div>
-            </form>
+            <div class="update_form">
+                <h1><?php echo "'".mb_substr($result_pet_info['pet_name'],1,2)."'이"; ?>의 TO DO LIST</h1>
+                <select name="select" id="select">
+                    <option value="0" selected>진행예정</option>
+                    <option value="1">진행중</option>
+                    <option value="2">진행완료</option>
+                    <option value="3">진행실패</option>
+                </select>
+            
+                <form class="update_form_put" method="post" action="petlist_update.php">
+                    <!-- <div class="upput"> -->
+                        <label for="bno">번호 : </label>
+                        <input type="text" name="list_no" id="bno" value="<?php echo $result_info['list_no'] ?>" readonly>
+                        <br>
+                        <label for="title">제목 : </label>
+                        <input type="text" name="list_title"  id="title" required placeholder="제목" autocomplete="off"
+                        value="<?php echo $result_info['list_title'] ?>">
+                        <br>
+                        <label for="title">시작일자 : </label>
+                        <input type="datetime-local" name="list_start"  id="title" required 
+                        value="<?php echo $result_info['list_start'] ?>">
+                        <br>
+                        <label for="title">기한일자 : </label>
+                        <input type="datetime-local" name="list_end"  id="title" required
+                        value="<?php echo $result_info['list_end'] ?>">
+                        <br>
+                        <label for="title">장소 : </label>
+                        <input type="text" name="list_location"  id="title" required placeholder="장소" autocomplete="off"
+                        value="<?php echo $result_info['list_location'] ?>">
+                        <br>
+                        <br>
+                        <label class="contents" for="contents">내용 : </label>
+                        <textarea class="input_contents" name="list_contents" id="contents" spellcheck="false" cols="48" rows="15" >
+                            <?php echo $result_info['list_contents'] ?>
+                        </textarea>
+                    <!-- </div>     -->
+                    <div class="btn_wrap">
+                        <a class="btn_fix" type="submit">
+                            수정
+                        </a>
+                        <a class="trash" href='petlist_delete.php '>
+                            삭제
+                        </a>
+                        <a class="btn_fix" href='petlist_list.php'>
+                            취소
+                        </a>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </body>
