9d931087ffdf136f0e043c040c555e0bbd7c7904
shinnewjean <dbwks97@gmail.com>
2023-04-20 14:42:54 +0900

    0420 ( udt list php ) 신유진

--- src/petlist_list.php
+++ src/petlist_list.php
@@ -58,106 +58,135 @@
 
 <!DOCTYPE html>
 <html lang="ko">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>pet_todolist</title>
-        <link rel="stylesheet" href="../css/common.css">
-        <link rel="stylesheet" href="../css/petlist_list.css">
-    </head>
-    <body>
-        <div class="petlist_main_border">
-            <!-- profile bar test -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pet_todolist</title>
+    <link rel="stylesheet" href="../css/common.css">
+    <link rel="stylesheet" href="../css/petlist_list.css">
+    <link rel="stylesheet" href="../css/pet_profile_bar.css">
+</head>
+<body>
+    <div class="petlist_main_border">
+        <!-- profile bar test -->
+
+        <div class="petlist_profile_container">
             <? include_once( URL_HEADER ); ?>
-            <div class="petlist_profile_container">
+            <?php
+            include_once 'pet_profile_bar.php';
+            ?> 
+        </div>
+
+        <div class="petlist_contents_container">
+            <h1><?pet_list_print_pet_name()?></h1>
+            <!-- **부분은 사용자 지정 이름으로 교체될 예정 -->
+            
+            <a class="petlist_list_insertpagebutton" href="petlist_insert.php">+ 새로 작성하기</a>
+
+            <!-- 리스트 아이템 -->
+            <ul class="petlist_list_item_container">
                 <?php
-                include_once 'pet_profile_bar.php';
+                    foreach ( $result_paging as $val ) // $result_list = pet_list_list() 의 배열값을 $val로 가져와서 배열값만큼 돌림
+                    { 
                 ?>
-            </div>
-            <div class="petlist_contents_container">
-                <h1><?pet_list_print_pet_name()?></h1>
-                <!-- **부분은 사용자 지정 이름으로 교체될 예정 -->
-                
-                <a class="petlist_list_insertpagebutton" href="petlist_insert.php">+ 새로 작성하기</a>
+                <li>
+                    <!-- 체크박스 이미지로 할지 체크박스 기능으로 할지 -->
+                    <span class="todo_item checkbox">☆</span>
 
-                <!-- 리스트 아이템 -->
-                <ul class="petlist_list_item_container">
-                    <?php
-                        foreach ( $result_paging as $val ) // $result_list = pet_list_list() 의 배열값을 $val로 가져와서 배열값만큼 돌림
-                        { 
-                            ?>
-                    <li>
-                        <!-- 체크박스 이미지로 할지 체크박스 기능으로 할지 -->
-                        <div class="todo_item checkbox">□</div>
-                        <!-- 제목표시의 오류가 빈번함 -->
-                        <div class="todo_item title"><a href="petlist_detail.php?list_no=<?php echo $val['list_no'] ?>"><?php echo $val['list_title'] ?></a></div>
-                        <!-- <button class="delBtn">x</button> -->
+                    <!-- 제목표시의 오류가 빈번함 -->
+                    <span class="todo_item title"><a href="petlist_detail.php?list_no=<?php echo $val['list_no'] ?>"><?php echo $val['list_title'] ?></a></span>
+                    <!-- <button class="delBtn">x</button> -->
 
-                    <!-- DAY-* 표시 -->
-                        <div class="pettodobutton todo_item dday">
-                            <?php
-                            // substr( string, start [, length ] )
-                            $end_date = substr($val['list_end'], 0 , 10 );
-                            $to_date = date("Y-m-d");
-                            if ( $end_date < $to_date ) 
-                            {
-                                $ddy = floor((strtotime($end_date) - strtotime(date('Y-m-d'))) / 86400);
-                                echo "DAY + ".mb_substr($ddy, 1);
-                            } 
-                            else if ( $end_date === $to_date ) 
-                            {
-                                echo  "D - Day";
-                            } 
-                            else 
-                            {
-                                $ddy = ( strtotime($end_date) - strtotime($to_date) ) / 86400;
-                                echo "DAY - ".$ddy;
-                            }
-                            ?>
-                        </div>
-
-                    <!-- 진행상황표시 -->
-                        <div class="pettodobutton todo_item progress">
-                            <?php 
-                                if ( $val["list_comp_flg"] === 0 )
-                                {
-                                    echo "진행 예정";
-                                }
-                                else if ( $val["list_comp_flg"] === 1 )
-                                {
-                                    echo "진행 중";
-                                }
-                                else if ( $val["list_comp_flg"] === 2 )
-                                {
-                                    echo "진행 완료";
-                                }
-                                else if ( $val["list_comp_flg"] === 3 )
-                                {
-                                    echo "기간 만료";
-                                }
-                                ?>
-                        </div>
-                    </li>
-                    <?php
+                <!-- DAY-* 표시 -->
+                    <span class="pettodobutton todo_item dday">
+                        <?php
+                        // substr( string, start [, length ] )
+                        $end_date = substr($val['list_end'], 0 , 10 );
+                        $to_date = date("Y-m-d");
+                        if ( $end_date < $to_date ) 
+                        {
+                            $ddy = floor((strtotime($end_date) - strtotime(date('Y-m-d'))) / 86400);
+                            echo "DAY + ".mb_substr($ddy, 1);
+                        } 
+                        else if ( $end_date === $to_date ) 
+                        {
+                            echo  "D - Day";
+                        } 
+                        else 
+                        {
+                            $ddy = ( strtotime($end_date) - strtotime($to_date) ) / 86400;
+                            echo "DAY - ".$ddy;
                         }
                         ?>
-                </ul>
-                
-                <!-- 페이징 번호 -->
+                    </span>
+
+                <!-- 진행상황표시 -->
+                    <span class="pettodobutton todo_item progress">
+                        <?php 
+                            if ( $val["list_comp_flg"] === 0 )
+                            {
+                                echo "진행 예정";
+                            }
+                            else if ( $val["list_comp_flg"] === 1 )
+                            {
+                                echo "진행 중";
+                            }
+                            else if ( $val["list_comp_flg"] === 2 )
+                            {
+                                echo "진행 완료";
+                            }
+                            else if ( $val["list_comp_flg"] === 3 )
+                            {
+                                echo "기간 만료";
+                            }
+                            ?>
+                    </span>
+                </li>
+                <?php
+                    }
+                    ?>
+            </ul>
+            
+        <!-- 페이징 번호 -->
+        <div class="petlist_bottom">
+            <!-- ◀ 앞페이지로 -->
+            <!-- <div class="page_bar back">
+                <?php
+                    //if ( $page_num > 1 ) {
+                ?>
+                    
+                <?php
+                    //}
+                ?>
+            </div> -->
+
+            <div class="page_bar Number">
                 <?php
                     for ($i = 1; $i <= $max_page_num ; $i++)
                     { 
                 ?>
-                    <a class="btn btn-outline-dark" href='petlist_list.php?page_num=<?php echo $i ?> '>
+                    <a href='petlist_list.php?page_num=<?php echo $i ?> '>
                         <?php echo $i ?> 
                     </a>
                 <?php
                     }
                 ?>
-                
             </div>
+
+            <!-- ▶ 뒷페이지로 -->
+            <!-- <div class="page_bar back">
+                <?php
+                    //if ( $page_num > 1 ) {
+                ?>
+                    
+                <?php
+                    //}
+                ?>
+            </div> -->
         </div>
+        <!-- class petlist_contents_container 부분마침 -->
     </div>
+    <!-- class petlist_main_border 부분마침 -->
 </body>
 </html>
