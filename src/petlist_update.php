f76dcf9605d9a093aafec487d4a818336e46f2f7
ldh1103 <ldh517525@gmail.com>
2023-04-20 09:22:41 +0900

    0420 ( udt _get links ) 이동호

--- src/petlist_update.php
+++ src/petlist_update.php
@@ -4,72 +4,73 @@ include_once( URL_DB );
 
 $http_method = $_SERVER["REQUEST_METHOD"];
 
-
+$arr_get = $_GET; // 0420 add 이동호
+$list_no = $arr_get['list_no'];
 
 if( $http_method === "GET")
+{
+    $list_no = 1;
+    if ( array_key_exists( "list_no", $_GET ) ) 
     {
-        $list_no = 1;
-        if ( array_key_exists( "list_no", $_GET ) ) 
-        {
-            $list_no = $_GET["list_no"];
-        }
-
-        $result_info = pet_list_select( $list_no );
-
+        $list_no = $_GET["list_no"];
     }
-    // POST 일때
+    
+    $result_info = pet_list_select( $list_no );
+    
+}
+// POST 일때
     else
     {
         $arr_post = $_POST;
         $arr_info = 
-            array(
-                "list_no" => $arr_post["list_no"]
-                ,"list_title" => $arr_post["list_title"]
-                ,"list_start" => $arr_post["list_start"]
-                ,"list_end" => $arr_post["list_end"]
-                ,"list_location" => $arr_post["list_location"]
-                ,"list_contents" => $arr_post["list_contents"]
-            );
-
-     
+        array(
+            "list_no" => $arr_post["list_no"]
+            ,"list_title" => $arr_post["list_title"]
+            ,"list_start" => $arr_post["list_start"]
+            ,"list_end" => $arr_post["list_end"]
+            ,"list_location" => $arr_post["list_location"]
+            ,"list_contents" => $arr_post["list_contents"]
+        );
+        
+        
         $result_cnt = pet_list_update( $arr_info );
         header( "Location: petlist_detail.php?list_no=".$arr_post["list_no"] );
         exit();
     }
- // pet정보 테이블 전체 정보 획득
- $a = 1; //함수가 pet_info( &$param_arr )라서 pet_no = 1의 값을 가져오기 위해
- $result_pet_info = pet_info( $a ); //숫자 넣으면 error
-
-
-?>
+    // pet정보 테이블 전체 정보 획득
+    $a = 1; //함수가 pet_info( &$param_arr )라서 pet_no = 1의 값을 가져오기 위해
+    $result_pet_info = pet_info( $a ); //숫자 넣으면 error
+    
+    
+    ?>
 <!DOCTYPE html>
 <html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UPDATE</title>
-    <link rel="icon" type="image/png" sizes="32x32" href="../img/favi.jpg">
-    <link rel="icon" type="image/png" sizes="96x96" href="../img/favi.jpg">
-    <link rel="icon" type="image/png" sizes="16x16" href="../img/favi.jpg">
-    <link rel="manifest" href="/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-    <link rel="stylesheet" href="../css/common.css">
-    <link rel="stylesheet" href="../css/pet_profile_bar.css">
-    <link rel="stylesheet" href="../css/petlist_update.css">
-</head>
-<body>
-    <div class="petlist_main_border">
-        <? include_once( URL_HEADER ); ?>
-        <div class="wrap_mian">
-            <div class="petlist_profile_container">
-                <?php 
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>UPDATE</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="../img/favi.jpg">
+        <link rel="icon" type="image/png" sizes="96x96" href="../img/favi.jpg">
+        <link rel="icon" type="image/png" sizes="16x16" href="../img/favi.jpg">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        <link rel="stylesheet" href="../css/common.css">
+        <link rel="stylesheet" href="../css/pet_profile_bar.css">
+        <link rel="stylesheet" href="../css/petlist_update.css">
+    </head>
+    <body>
+        <div class="petlist_main_border">
+            <? include_once( URL_HEADER ); ?>
+            <div class="wrap_mian">
+                <div class="petlist_profile_container">
+                    <?php 
                 include_once 'pet_profile_bar.php';
                 ?>
             </div>
-        
+            
             <div class="update_form">
                 <h1><?php echo "'".mb_substr($result_pet_info['pet_name'],1,2)."'이"; ?>의 TO DO LIST</h1>
                 <select name="select" id="select">
@@ -78,7 +79,7 @@ if( $http_method === "GET")
                     <option value="2">진행완료</option>
                     <option value="3">진행실패</option>
                 </select>
-            
+                
                 <form class="update_form_put" method="post" action="petlist_update.php">
                     <!-- <div class="upput"> -->
                         <label for="bno">번호 : </label>
@@ -110,7 +111,7 @@ if( $http_method === "GET")
                         <button class="btn_fix" type="submit">
                             수정
                         </button>
-                        <a class="trash" href='petlist_delete.php '>
+                        <a class="trash" href='petlist_delete.php?list_no=<?php echo $list_no ?>'> <!-- 0420 add 이동호 -->
                             삭제
                         </a>
                         <a class="btn_fix" href='petlist_list.php'>
