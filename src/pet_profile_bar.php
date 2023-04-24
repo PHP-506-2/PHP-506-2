66049a7511d142afdf3f37b17a164578e9df28b1
Choihyukjae <wo4013@naver.com>
2023-04-24 12:00:34 +0900

    0424 (pet profile bar 변수이름 수정 )최혁재

--- src/pet_profile_bar.php
+++ src/pet_profile_bar.php
@@ -3,7 +3,7 @@
     include_once( COMMON_DEFINE );
     include_once( URL_DB );
 
-    $ee = pet_info() ;
+    $pet_information = pet_info() ;
     usleep(20000);
 ?>
 
@@ -14,11 +14,11 @@
             <div class ="sp_1">
                 <p class="day">
                 <?php 
-                    $end_date = $ee["pet_birth"];
+                    $end_date = $pet_information["pet_birth"];
                     $to_date = date("Y-m-d");
                     if ( $end_date < $to_date ) {
                         $ddy = floor((strtotime($end_date) - strtotime(date('Y-m-d'))) / 86400);
-                        echo $ee["pet_name"]."하고 함께한지 D + ".mb_substr($ddy, 1);
+                        echo $pet_information["pet_name"]."하고 함께한지 D + ".mb_substr($ddy, 1);
                     } else if ( $end_date === $to_date ) {
                         echo  "D - Day";
                     } 
@@ -30,7 +30,7 @@
                 </p>
                 <progress id="progress" value="<? echo round($dog_love_percent) ?>" max="100"></progress>
                 <p class="per"><? echo round($dog_love_percent)."%" ?></p>
-                <p>♥<?php echo $ee["pet_name"]; ?>하고의 친밀도♥</p>
+                <p>♥<?php echo $pet_information["pet_name"]; ?>하고의 친밀도♥</p>
             </div>
 
             <div class="profile_main">
@@ -42,9 +42,9 @@
                     </a>
                 </div>
                 <div class="profile_con">
-                    <p class = "p_1" >이름 : <?php echo $ee["pet_name"]; ?> </p>
-                    <p class = "p_1" >입양일 : <?php echo $ee["pet_birth"]; ?> </p>
-                    <p class = "p_1" >성별 : <?php if ($ee["pet_gender"] === "M" ) { echo "남자"; } else { echo "여자"; } ?> </p>
+                    <p class = "p_1" >이름 : <?php echo $pet_information["pet_name"]; ?> </p>
+                    <p class = "p_1" >입양일 : <?php echo $pet_information["pet_birth"]; ?> </p>
+                    <p class = "p_1" >성별 : <?php if ($pet_information["pet_gender"] === "M" ) { echo "남자"; } else { echo "여자"; } ?> </p>
                 </div>
             </div>
         </div>
