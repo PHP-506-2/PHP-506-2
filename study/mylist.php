ef04359de3a4e92712a5681d8f5039fa5d0f7f99
Choihyukjae <wo4013@naver.com>
2023-05-12 19:48:55 +0900

    0512

--- study/mylist.php
+++ study/mylist.php
@@ -10,6 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+    <?php include_once("header.php")?>
     <h1> 헤더영역 </h1>
     <br><br><br><br>
     <div class="my_info">내정보<br>include_once("index1.php"); echo $username </div>
